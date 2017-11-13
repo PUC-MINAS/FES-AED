ca11df69d639782758f6ca4aad514d9b9f5a6b2c
lebanam <33206688+lebanam@users.noreply.github.com>
2017-11-13 12:03:39 -0200

    teste2

--- Prototipo/excluireserva/excluireserva/main.c
+++ Prototipo/excluireserva/excluireserva/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /*Nesta opção o programa principal deverá chamar uma função passando, além das
 estruturas de dados necessárias, o número do quarto, o período de permanência e
 o CPF do passageiro.
 A função deverá retornar um código que:
 1 - indica período inválido;
 
-Caso seja feita a exclusão, deverá atualizar o número de vagas no hotel ao longo
-de todo o período, excluindo o número do CPF do cliente nos dias referentes ao
+Caso seja feita a exclusão,
+deverá atualizar o número de vagas no hotel ao longo de todo o período,
+excluindo o número do CPF do cliente nos dias referentes ao
 período naquele quarto.*/
 
 
-struct hotel {
+struct hotel
+{
     int quarto[6][7], preco[4], dia[31][6];
     long cpf[31][6];
     short camas[6][7];
 };
 typedef struct hotel htl;
 
-void excluir_reserva(int h.quarto[6][7], int h.dia[31][6], int periodo, int h.cpf[6][7])
-{
-    int a,q,cpf,cont,andar,quarto,vagas;
+void excluir_reserva(int h.quarto[6][7], int h.dia[31][6], int periodo, int h.cpf[6][7]){
+    int a,q,d,m,cpf,cont,andar,quarto,vagas;
     vagas=0;
     printf("Digite o periodo da estadia: ");
     scanf("%d",&periodo);
@@ -28,22 +30,25 @@ void excluir_reserva(int h.quarto[6][7], int h.dia[31][6], int periodo, int h.cp
     scanf("%d",&cpf);
     cont=0;
 
+//0 - indica que o quarto não existe;
     printf("Digite andar do quarto: ");
     scanf("%d",&andar);
 
     printf("Digite numero do quarto: ");
     scanf("%d",&quarto);
 
-    //0 - indica que o quarto não existe;
-    if(andar>6 or andar<1 or quarto>7 or quarto<1){
-            printf("0 - Quarto não existe! ");
-
+    if(andar>6 or andar<1 or quarto>7 or quarto<1)
+    {
+        printf("0 - Quarto não existe! ");
     }
 
-    for (a=0;a<6;a++){
-        for(q=0;q<7;q++){
+    for (a=0; a<6; a++)
+    {
+        for(q=0; q<7; q++)
+        {
 
-            if(cpf==h.cpf[a][q]){
+            if(cpf==h.cpf[a][q])
+            {
                 cont=1;
                 h.cpf=0;
                 else
@@ -52,20 +57,28 @@ void excluir_reserva(int h.quarto[6][7], int h.dia[31][6], int periodo, int h.cp
 
         }
     }
-    for (a=0;a<6;a++){
-        for(q=0;q<7;q++){
+                  if(cont==1)
+                {
+                    //3 - indica exclusão realizada com sucesso;
+                    printf("3 - Exclusão realizada com sucesso!");
+                    else
+                    //2 - indica reserva inexistente no período;
+                        printf("2 - Reserva inexistente. ");
+                }
+            }
 
+//atualizar o número de vagas no hotel;
+    for (a=0; a<6; a++){
+        for(q=0; q<7; q++){
+            for (m=0; m<6; m++){
+                for(d=0; d<31; d++){
+                   if(cpf[d][m]==0){
+                    vagas++;
+                   }
+                }
+            }
         }
-
-
-    if(cont==1){
-            //3 - indica exclusão realizada com sucesso;
-        printf("3 - Exclusão realizada com sucesso!");
-        else
-            //2 - indica reserva inexistente no período;
-                printf("2 - Reserva inexistente. ");
     }
-}
 
 
 
@@ -80,9 +93,9 @@ int main()
     h.preco[2]=150;
     h.preco[3]=180;
 
-    for(a=0;a<6;a++)
+    for(a=0; a<6; a++)
     {
-        for(q=0;q<7;q++)
+        for(q=0; q<7; q++)
         {
             h.quarto[a][q]=q+apt;
             h.camas[a][q]= rand()%4;
@@ -96,10 +109,10 @@ int main()
         printf("\n");
     }
     int dia,mes;
-    for(a=0;a<6;a++)
+    for(a=0; a<6; a++)
     {
         mes=a+1;
-        for(q=0;q<31;q++)
+        for(q=0; q<31; q++)
         {
             dia=q+1;
 
@@ -107,7 +120,7 @@ int main()
         }
         printf("\n");
     }
-    excluir_reserva(reserva, num_quarto, dia, mes, ano, periodo );
+    excluir_reserva(reserva,num_quarto, dia, mes, ano, periodo );
 
     return 0;
 }
